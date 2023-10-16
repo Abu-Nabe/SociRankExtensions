c5053a0b93fac317d6c1eb7ba496cc223b67229c
Abu <118349590+shadowfsdev@users.noreply.github.com>
2023-10-16 19:16:10 +1100

    Delete Javascript/uniqueID.js

--- Javascript/uniqueID.js
+++
@@ -1,9 +0,0 @@
-function generateUniqueId() {
-    const timestamp = Date.now().toString();
-    const random1 = Math.floor(Math.random() * 1000000).toString().padStart(7, "0");
-    const random2 = Math.floor(Math.random() * 1000000).toString().padStart(7, "0");
-  
-    return `${timestamp}-${random1}-${random2}`;
-  }
-  
-export const uniqueId = generateUniqueId();
